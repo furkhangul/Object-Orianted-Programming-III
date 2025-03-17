37aca6d7465eabb4a48a599cdc8484b336f6fc14
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-03-17 11:39:40 +0300

    3

---
+++ Dizi Tanımı.py
@@ -0,0 +1,17 @@
+
+
+    """
+    Pythonda verileri saklamak için farklı yollar vardır. Tuple, Data, Koleksiyon, Dictionary gibi farklı türler vardır.
+    Dictionary sözlük dizimi anahtar kilit uyumu ile hareket eder. Tuple mantığı ise bu zorunluluğu kaldırır. Anahtar olur fakat kilit kullanılmaz.
+    Data sözlük dizimi ise yalnızca verilerin saklanması için kullanılır.
+    """
+
+    dizi = ["ELMA", "ARMUT", "NAR"]
+
+    print(dizi[0])
+    print(dizi[1])
+
+    x = "Python"
+    y = "Nesne Yönelimli"
+    z = "Programlama Dilidir."
+    print(x,y,z)
