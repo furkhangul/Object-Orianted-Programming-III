dca9192e3f14394257981b10a9959781b4d1a6bc
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-03-17 11:33:34 +0300

    2

---
+++ Basit İf else.py
@@ -0,0 +1,13 @@
+"""
+Python'da diğer yazılım dillerinde olduğu gibi sürekli parantez açıp kapama gibi bir alışkanlık yoktur. Bunun için çift nokta kullanılır ve
+satır aralığı kessinlikle çok önemlidir.
+
+if else gibi en temel komutların kullanımında da kessinikle boşlukların önemi çok büyüktür.
+"""
+
+sayi1 = int(input("Sayı 1:"))
+sayi2 = int(input("Sayı 2:"))
+if sayi1> sayi2:
+    print("Merhaba Dünya !")
+else :
+    print("Hoşçakal Dünya !")
