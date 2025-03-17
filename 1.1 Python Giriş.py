dd650c82fb7902d81c7ab3fdce06ff83fef95c72
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-03-17 11:29:16 +0300

    1

---
+++ 1.1 Python Giriş.py
@@ -0,0 +1,27 @@
+"""
+*Python ile veritabanı dosyaları okunabilir hatta bu dosyalar bağlanabilir.
+*Python ile web sistemlerinin birbirine bağlanması ve sunucudan veri aktarma işlemleri gerçekleştirilebilir.
+*İş akışları ile birilikte kullanılarak yazılımda aktif görev alabilir.
+*Büyük verilerin işlenemesi ve karmaşık matematik işlemlerinin çözülmesi için kullanılabilir.
+*Hızlı prototip oluşturulmasında ve bu prototiplerin hız bakımından diğer diller ile yarışabilecek seviyede olması için python lullanışabilir.
+"""
+
+
+
+
+"""
+* Python farklı platformlarda çalışabilir.
+* İngilizce diline çok yakın olduğundan dolayı dilin öğrenilmesinde sıkıntı çekilmez.
+* Basit söz dizinimlerine sahiptir.
+* Daha az satırlı kodlar ile çok iş yapılmasını baz aldığından kullanılması bir o kadar basittir.
+* Halihazırda bir yorumlayıcı ile çalıştığından kodun yazılır yazılmaz çalışamsı bir sorun teşkil etmemektedir.
+* Nesne yönelimli veya işlevsel olarak ele alınabilir.
+"""
+
+
+"""
+Python kullanılabilmesi için öncelikle python.com/downloads kısmından en güncel python versiyonunun indirilmesi gerekmektedir.
+
+
+versiyon kontrolü için : python --verison
+"""
