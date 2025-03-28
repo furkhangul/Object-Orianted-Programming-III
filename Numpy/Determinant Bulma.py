940ade6f83c8c366e657eaa45fc0f1602876de47
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-03-28 15:33:26 +0300

    Create Determinant Bulma.py

---
+++ Numpy/Determinant Bulma.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def matrisolustur():
+    matris = np.zeros((3,3))
+    for i in range(3):
+        for j in range(3):
+            matris[i][j] = int(input(f"{i+1}.sütunun {j+1}.satırını giriniz:"))
+    return matris
+def determinant(matris):
+    det = np.linalg.det(matris)
+    return det
+
+matris = matrisolustur()
+det = determinant(matris)
+
+print("Matrisiniz:")
+print(matris)
+print(f"Matrisinizin determinantı: {det}")
