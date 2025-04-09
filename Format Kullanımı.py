a9392f473f94c25878deafc68cebbea0bc25d308
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-04-09 10:21:24 +0300

    .

---
+++ Format Kullanımı.py
@@ -0,0 +1,16 @@
+"""
+Format yönteminin kullanımı
+"""
+
+Age = "21"
+
+text = "yaşınız: " + Age
+
+print(text)
+
+# Format yöntemini kullanarakta değiştirebiliriz.
+age=21
+
+text = "Hello my age is {}"
+
+print(text.format(age))
