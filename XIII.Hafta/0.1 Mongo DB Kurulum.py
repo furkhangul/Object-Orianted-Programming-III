d2c96cea0a33284f9ae589c8912c268668a68af6
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2025-05-30 11:17:20 +0300

    1

---
+++ XIII.Hafta/0.1 Mongo DB Kurulum.py
@@ -0,0 +1,43 @@
+#Mongo DB
+
+"""
+En popüler NoSQL veritabanlarından biridir.
+Verileri klasik tablo satır - sütun yapısı yerine json benzeri belgelerde depolayan bir veritabanıdır.
+Esnektir.
+Ölçeklendirilebilir: Büyük veri uygulamalarında yatay ölçeklendirme kolaydır.
+Belge tabanlıdır: Her kayıt, bağımsız bir belge olarak saklanır.
+"""
+
+#İndirme için:
+"""
+pip modülü ile indirme işlemi gerçekleşitirilebilir.
+pip install pymongo
+"""
+
+
+
+
+"""
+Mongo veri tabanına erişilebilmesi için MongoDB sürücüsüne ihtiyaç duyar.
+
+MongoDB’yi kullanabilmek için iki farklı yol vardır:
+
+1. Yerel (Local) Kurulum
+MongoDB'yi bilgisayarınıza indirip kurabilirsiniz:
+
+https://www.mongodb.com/try/download/community
+
+Kurulumdan sonra veritabanı sunucusunu çalıştırmanız gerekir (mongod komutu ile).
+
+2. Bulut (Cloud) Üzerinden Kullanım - MongoDB Atlas
+Hiçbir şey kurmadan doğrudan internet üzerinden MongoDB kullanmak isterseniz, MongoDB’nin sunduğu ücretsiz bulut hizmeti tam size göre:
+
+https://www.mongodb.com/cloud/atlas
+
+Atlas üzerinden veritabanı oluşturabilir, kullanıcı tanımlayabilir ve bağlantı bilgilerinizi alabilirsiniz.
+"""
+
+#import pymongo
+
+
+
